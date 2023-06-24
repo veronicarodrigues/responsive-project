fbcfd01b27d34ee672c1b8ef662bbec674226175
veronicarodrigues <ra.veronica14@gmail.com>
2023-06-24 21:42:19 +0800

    Work section functional arrows

---
+++ app.js
@@ -0,0 +1,8 @@
+function leftScroll() {
+  const left = document.querySelector(".row-work");
+  left.scrollBy(-260, 0);
+}
+function rightScroll() {
+  const right = document.querySelector(".row-work");
+  right.scrollBy(260, 0);
+}
